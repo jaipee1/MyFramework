b2d3e42321558db5e3bdad04884f9f06ec99683c
Jai Prakash <yadavjaiprakash18@gmail.com>
2020-03-14 23:12:06 +0530

    Initial commit

---
+++ MyFramwork/MyFramework.swift
@@ -0,0 +1,13 @@
+//
+//  MyFramwork.swift
+//  MyFramwork
+//
+//  Created by jaipee on 14/03/20.
+//  Copyright © 2020 jaipee. All rights reserved.
+//
+
+import Foundation
+public class MyFramework {
+  static let shared = MyFramework()
+  func foo() {}
+}
---
+++ MyFramwork/MyFramwork.h
@@ -0,0 +1,19 @@
+//
+//  MyFramwork.h
+//  MyFramwork
+//
+//  Created by jaipee on 14/03/20.
+//  Copyright © 2020 jaipee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyFramwork.
+FOUNDATION_EXPORT double MyFramworkVersionNumber;
+
+//! Project version string for MyFramwork.
+FOUNDATION_EXPORT const unsigned char MyFramworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFramwork/PublicHeader.h>
+
+
